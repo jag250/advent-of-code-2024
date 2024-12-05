210f7b05c9c1af289ac66b80a16947bae4f19241
Jadon Gaertner <jgaertne@usc.edu>
2024-12-05 08:54:11 -0800

    day 5 and optional auto-submit

--- advent_of_code_2024/__main__.py
+++ advent_of_code_2024/__main__.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 
 from advent_of_code_2024.get_data import add_data_to_file
+import aocd
 
 def dynamic_import_day(day):
     try:
@@ -16,6 +17,14 @@ def dynamic_import_day(day):
 @click.command()
 @click.argument("part", type=click.IntRange(min=1, max=2))
 @click.argument("day", type=click.IntRange(min=1, max=25), default=lambda: datetime.now(ZoneInfo("America/New_York")).day)
-def main(part: int, day: int):
+@click.option("-s", "--submit", is_flag=True, default=False)
+def main(part: int, day: int, submit: bool):
     add_data_to_file(day=day)
-    print(dynamic_import_day(day)(part))
+    sol = dynamic_import_day(day)(part)
+    print(sol)
+
+    part_num_to_string = {1:"a", 2:"b"}
+    if sol and submit:
+        print("Submitting...")
+        aocd.submit(sol, part=part_num_to_string[part], day=day, year=2024)
+
--- advent_of_code_2024/day5/day5.py
+++ advent_of_code_2024/day5/day5.py
@@ -1,5 +1,7 @@
 import os
 
+from functools import cmp_to_key
+
 def day5(part: int):
     current_dir = os.path.dirname(__file__)
     file_path = os.path.join(current_dir, "day5.txt")
@@ -13,10 +15,56 @@ def day5(part: int):
 
 def part1(f):
     res = 0
-    return res
+    d = {}
+    for l in f:
+        if l == "\n":
+            break
+
+        a, b = map(int, l.strip().split("|"))
+        d.setdefault(a, set()).add(b)
+
+    for l in f:
+        if l == "\n" or "|" in l:
+            continue
+
+        a = [int(s) for s in l.split(",")]
+        m = set()
+        for i in a:
+            if i in d.keys() and d[i] & m:
+                break
+            
+            m.add(i)
+            
+        else:
+            res += a[len(a)//2]
                 
+    return res
+
 
 def part2(f):
     res = 0
+    d = {}
+    for l in f:
+        if l == "\n":
+            break
+
+        a, b = map(int, l.strip().split("|"))
+        d.setdefault(a, set()).add(b)
+
+
+    for l in f:
+        if l == "\n" or "|" in l:
+            continue
+
+        a = [int(s) for s in l.split(",")]
+        m = set()
+        for i in a:
+            if i in d.keys() and d[i] & m:
+                a.sort(key=cmp_to_key(lambda n, m: -1 if m in d[n] else 1 if n in d[m] else 0))
+                res+=a[len(a)//2]
+                break
+            m.add(i)
+
+    
     return res
 
