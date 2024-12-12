73e988b9125e8a56f35f7893b1cbb117d21b9cb7
Jadon Gaertner <jgaertne@usc.edu>
2024-12-11 23:38:36 -0800

    fell behind, slowly catching up

--- advent_of_code_2024/day6/day6.py
+++ advent_of_code_2024/day6/day6.py
@@ -1,5 +1,7 @@
 import os
 
+from copy import deepcopy
+
 def day6(part: int):
     current_dir = os.path.dirname(__file__)
     file_path = os.path.join(current_dir, "day6.txt")
@@ -10,13 +12,90 @@ def day6(part: int):
         else:
             return part2(f)
 
+def walk(grid, i, j, dx, dy):
+    cnt = 0
+    cont = True
+    while True:
+        if not 0<=i<len(grid) or not 0<=j<len(grid[0]):
+            cont = False
+            break
+
+        if grid[i][j] == '#':
+            i -= dx
+            j -= dy
+            break
+
+        if grid[i][j] == '.':
+            grid[i][j] = 'x'
+            cnt += 1
+        
+        i += dx
+        j += dy
+
+    
+    return cnt, cont, i, j
+
+DIRS = ((-1, 0), (0, 1), (1, 0), (0, -1))
 
 def part1(f):
-    res = 0
+    res = 1
+    grid = [list(line.strip()) for line in f]
+    x = 0
+    y = 0
+    for i, a in enumerate(grid):
+        if '^' in a:
+            y = a.index('^')
+            x = i
+            break
+
+    dir_idx = 0
+    cont = True    
+    while cont:
+        cnt, cont, x, y = walk(grid, x, y, DIRS[dir_idx][0], DIRS[dir_idx][1])
+        dir_idx = (dir_idx+1)%4
+        res += cnt
+
+
     return res
                 
 
 def part2(f):
     res = 0
+    grid = [list(line.strip()) for line in f]
+    initial_x = 0
+    initial_y = 0
+    for i, a in enumerate(grid):
+        if '^' in a:
+            initial_y = a.index('^')
+            initial_x = i
+            break
+
+    start_and_end_coords = set()
+    for block_i, a in enumerate(grid):
+        for block_j, _ in enumerate(a):
+            x_start = initial_x
+            y_start = initial_y
+            dir_idx = 0
+            if grid[block_i][block_j] == '#':
+                continue
+            
+            grid[block_i][block_j] = '#'
+            cont = True
+            while cont:
+                _, cont, new_x, new_y = walk(grid, x_start, y_start, DIRS[dir_idx][0], DIRS[dir_idx][1])
+                dir_idx = (dir_idx+1)%4
+                if (x_start, y_start, new_x, new_y) in start_and_end_coords:
+                    res += 1
+                    break
+
+                start_and_end_coords.add((x_start, y_start, new_x, new_y))
+                x_start = new_x
+                y_start = new_y
+            
+            start_and_end_coords.clear()
+            grid[block_i][block_j] = '.'
+
+
+
     return res
 
--- advent_of_code_2024/day7/day7.py
+++ advent_of_code_2024/day7/day7.py
@@ -9,14 +9,54 @@ def day7(part: int):
         
         else:
             return part2(f)
+        
+def concat(n, m):
+    return int(str(n)+str(m))
+
+
+def part1_helper(eqn, curr_num, eqn_idx):
+    if eqn_idx == len(eqn):
+        if curr_num == eqn[0]:
+            return True
+        
+        else:
+            return False
+
+    if curr_num > eqn[0]:
+        return False
+    
+    return part1_helper(eqn, curr_num * eqn[eqn_idx], eqn_idx+1) or part1_helper(eqn, curr_num + eqn[eqn_idx], eqn_idx+1)
+    
 
 
 def part1(f):
     res = 0
+    for l in f:
+        eqn = [int(i) for i in l.replace(":", "").split(" ")]
+        if part1_helper(eqn, eqn[1]*eqn[2], 3) or part1_helper(eqn, eqn[1]+eqn[2], 3):
+            res += eqn[0]
+
     return res
+
+def part2_helper(eqn, curr_num, eqn_idx):
+    if eqn_idx == len(eqn):
+        if curr_num == eqn[0]:
+            return True
+        
+        else:
+            return False
+
+    if curr_num > eqn[0]:
+        return False
+    
+    return part2_helper(eqn, curr_num * eqn[eqn_idx], eqn_idx+1) or part2_helper(eqn, curr_num + eqn[eqn_idx], eqn_idx+1) or part2_helper(eqn, concat(curr_num, eqn[eqn_idx]), eqn_idx+1)
                 
 
 def part2(f):
     res = 0
+    for l in f:
+        eqn = [int(i) for i in l.replace(":", "").split(" ")]
+        if part2_helper(eqn, eqn[1]*eqn[2], 3) or part2_helper(eqn, eqn[1]+eqn[2], 3) or part2_helper(eqn, concat(eqn[1], eqn[2]), 3):
+            res += eqn[0]
     return res
 
